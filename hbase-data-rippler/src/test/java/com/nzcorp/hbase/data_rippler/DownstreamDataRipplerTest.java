0ff2c522030a9654dbce7e0d984aedc218f74018
Steen Manniche <steen@manniche.net>
2017-01-01 19:06:15 +0100

    #270 adding tests for data rippler

--- hbase-data-rippler/src/test/java/com/nzcorp/hbase/data_rippler/DownstreamDataRipplerTest.java
+++ hbase-data-rippler/src/test/java/com/nzcorp/hbase/data_rippler/DownstreamDataRipplerTest.java
@@ -22,7 +22,7 @@ public class DownstreamDataRipplerTest {
     private HTable primaryTable;
     private HTable otherTable;
     private HTable secondaryIdxTable;
-    
+
     private static final byte[] family = new byte[] { 'e' };
 
     private static boolean[] completed = new boolean[1];
@@ -50,10 +50,18 @@ public class DownstreamDataRipplerTest {
 	testingUtility.getConfiguration().setStrings("secondary_index_table", "genome_index");
         testingUtility.getConfiguration().setStrings("source_column_family", "e");
 	testingUtility.getConfiguration().setStrings("target_column_family", "eg");
-        testingUtility.getConfiguration().setStrings("secondary_idx_cf", "a");
+        testingUtility.getConfiguration().setStrings("secondary_index_cf", "a");
         testingUtility.startMiniCluster();
+	System.out.println("Creating genome table");
         primaryTable = (HTable) testingUtility.createTable(primaryTableName, "e");
-        otherTable = (HTable) testingUtility.createTable(otherTableName, "eg");
+	System.out.println("Creating assembly table" );
+
+	byte[][] ba = new byte[2][];
+	ba[0] = "e".getBytes();
+	ba[1] = "eg".getBytes();
+
+        otherTable = (HTable) testingUtility.createTable(otherTableName, ba);
+	System.out.println("Creating secondary index table");
         secondaryIdxTable = (HTable) testingUtility.createTable(secondaryIdxTableName, "a");
     }
 
@@ -66,37 +74,84 @@ public class DownstreamDataRipplerTest {
     }
 
 
-    /**
-     * maybe: https://hbase.apache.org/testdevapidocs/src-html/org/apache/hadoop/hbase/coprocessor/TestRegionObserverStacking.ObserverB.html
-     * https://github.com/sematext/HBaseHUT/blob/master/src/test/java/com/sematext/hbase/hut/cp/TestHBaseHutCps.java
-     * https://qnalist.com/questions/136205/hbase-coprocessor-unit-testing
-     * https://github.com/apache/hbase/blob/master/hbase-endpoint/src/test/java/org/apache/hadoop/hbase/coprocessor/TestRegionServerCoprocessorEndpoint.java
-     * @throws Exception
-     */
     @Test
     public void postPutHappyCase() throws Exception {
 
-	//populate secondary index
+	//populate assembly table
+	Put assIdx = new Put("EFB1".getBytes());
+	assIdx.addColumn("e".getBytes(), "genome_accession_number".getBytes(), "EFG1".getBytes());
+	otherTable.put(assIdx);
+	otherTable.flushCommits();
+
+	//simulate population of secondary index as a result of the above
 	Put idxPut = new Put("EFG1".getBytes());
-	idxPut.addColumn("a".getBytes(), "genome_accesion_number".getBytes(), "EFB1".getBytes());
+	idxPut.addColumn("a".getBytes(), "EFB1".getBytes(), "".getBytes());
 	secondaryIdxTable.put(idxPut);
 	secondaryIdxTable.flushCommits();
-	
+
         Put tablePut = new Put("EFG1".getBytes());
-        tablePut.addColumn( "e".getBytes(), "genome_accession_number".getBytes(), "EFG1".getBytes());
+        tablePut.addColumn( "e".getBytes(), "some_key".getBytes(), "some_value".getBytes());
         primaryTable.put(tablePut);
         primaryTable.flushCommits();
 
         Get get = new Get("EFB1".getBytes());
         Result result = otherTable.get(get);
         List<Cell> cells = result.listCells();
+        Cell cell = cells.get(1);
+        String ck = new String(CellUtil.cloneRow(cell));
+        String cf = new String(CellUtil.cloneFamily(cell));
+        String cq = new String(CellUtil.cloneQualifier(cell));
+	String vl = new String(CellUtil.cloneValue(cell));
+
+        Assert.assertEquals( "We expect the column family from the rippled table","eg", cf);
+        Assert.assertEquals( "We expect the column to contain the column value from the target table", "some_key", cq);
+        Assert.assertEquals( "We expect the rowkey in the rippled table to be a EFB1", "EFB1", ck);
+	Assert.assertEquals( "We expect the value in the rippled table to contain the value form the parent", "some_value", vl);
+    }
+
+    @Test
+    public void postPutGetInterpretation() throws Exception {
+
+	//populate assembly table
+	Put assIdx = new Put("EFB10L".getBytes());
+	assIdx.addColumn("e".getBytes(), "genome_accession_number".getBytes(), "EFG1".getBytes());
+	otherTable.put(assIdx);
+	otherTable.flushCommits();
+
+	//simulate population of secondary index as a result of the above
+	Put idxPut = new Put("EFG1".getBytes());
+	idxPut.addColumn("a".getBytes(), "EFB10L".getBytes(), "".getBytes());
+	secondaryIdxTable.put(idxPut);
+	secondaryIdxTable.flushCommits();
+
+        Put tablePut = new Put("EFG1".getBytes());
+        tablePut.addColumn( "e".getBytes(), "some_key".getBytes(), "some_value".getBytes());
+        primaryTable.put(tablePut);
+        primaryTable.flushCommits();
+
+        Get get = new Get("EFB10L".getBytes());
+        Result result = otherTable.get(get);
+        List<Cell> cells = result.listCells();
         Cell cell = cells.get(0);
         String ck = new String(CellUtil.cloneRow(cell));
         String cf = new String(CellUtil.cloneFamily(cell));
         String cq = new String(CellUtil.cloneQualifier(cell));
+        String vl = new String(CellUtil.cloneValue(cell));
 
-        Assert.assertEquals( "We expect the column family from the rippled table","eg", cf);
+        Assert.assertEquals( "We expect the rowkey in the rippled table to be the source rowkey", "EFB10L", ck);
+        Assert.assertEquals( "We expect the column family from the rippled table","e", cf);
         Assert.assertEquals( "We expect the column to contain the rowkey for a target table", "genome_accession_number", cq);
-        Assert.assertEquals( "We expect the rowkey in the rippled table to be the source rowkey", "EFG1", ck);
+	Assert.assertEquals( "We expect the value in the rippled table to contain the value form the parent", "EFG1", vl);
+
+	cell = cells.get(1);
+        ck = new String(CellUtil.cloneRow(cell));
+        cf = new String(CellUtil.cloneFamily(cell));
+        cq = new String(CellUtil.cloneQualifier(cell));
+	vl = new String(CellUtil.cloneValue(cell));
+
+        Assert.assertEquals( "We expect the rowkey in the rippled table to be the source rowkey", "EFB10L", ck);
+        Assert.assertEquals( "We expect the column family from the rippled table","eg", cf);
+        Assert.assertEquals( "We expect the column to contain the rowkey for a target table", "some_key", cq);
+	Assert.assertEquals( "We expect the value in the rippled table to contain the value form the parent", "some_value", vl);
     }
 }
