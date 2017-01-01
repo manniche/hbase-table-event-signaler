df499b329983001e437e5f82f2240f6dc49a9ee1
Steen Manniche <steen@manniche.net>
2017-01-01 13:05:23 +0100

    #270 tests for the data rippler as well

---
+++ hbase-data-rippler/src/test/java/com/nzcorp/hbase/data_rippler/DownstreamDataRipplerTest.java
@@ -0,0 +1,102 @@
+package com.nzcorp.hbase.data_rippler;
+
+import org.apache.avro.generic.GenericData;
+import org.apache.hadoop.hbase.*;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.coprocessor.CoprocessorHost;
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class DownstreamDataRipplerTest {
+
+    private static final TableName primaryTableName = TableName.valueOf("genome");
+    private static final TableName otherTableName = TableName.valueOf("assembly");
+    private static final TableName secondaryIdxTableName = TableName.valueOf("genome_index");
+    private HTable primaryTable;
+    private HTable otherTable;
+    private HTable secondaryIdxTable;
+    
+    private static final byte[] family = new byte[] { 'e' };
+
+    private static boolean[] completed = new boolean[1];
+
+    private HBaseTestingUtility testingUtility = new HBaseTestingUtility();
+
+    @BeforeClass
+    public static void beforeClass() {
+        List<Logger> loggers = Collections.<Logger>list(LogManager.getCurrentLoggers());
+        loggers.add(LogManager.getRootLogger());
+        for (Logger logger : loggers) {
+            logger.setLevel(Level.OFF);
+        }
+    }
+
+    @Before
+    public void before() throws Exception {
+	/*
+	 * Load the coprocessor on the region
+	 */
+        testingUtility.getConfiguration().setStrings(
+                CoprocessorHost.USER_REGION_COPROCESSOR_CONF_KEY,
+                DownstreamDataRippler.class.getName());
+        testingUtility.getConfiguration().setStrings("destination_table", "assembly");
+	testingUtility.getConfiguration().setStrings("secondary_index_table", "genome_index");
+        testingUtility.getConfiguration().setStrings("source_column_family", "e");
+	testingUtility.getConfiguration().setStrings("target_column_family", "eg");
+        testingUtility.getConfiguration().setStrings("secondary_idx_cf", "a");
+        testingUtility.startMiniCluster();
+        primaryTable = (HTable) testingUtility.createTable(primaryTableName, "e");
+        otherTable = (HTable) testingUtility.createTable(otherTableName, "eg");
+        secondaryIdxTable = (HTable) testingUtility.createTable(secondaryIdxTableName, "a");
+    }
+
+    @After
+    public void after() throws Exception {
+        primaryTable = null;
+        otherTable = null;
+        testingUtility.shutdownMiniCluster();
+        testingUtility = null;
+    }
+
+
+    /**
+     * maybe: https://hbase.apache.org/testdevapidocs/src-html/org/apache/hadoop/hbase/coprocessor/TestRegionObserverStacking.ObserverB.html
+     * https://github.com/sematext/HBaseHUT/blob/master/src/test/java/com/sematext/hbase/hut/cp/TestHBaseHutCps.java
+     * https://qnalist.com/questions/136205/hbase-coprocessor-unit-testing
+     * https://github.com/apache/hbase/blob/master/hbase-endpoint/src/test/java/org/apache/hadoop/hbase/coprocessor/TestRegionServerCoprocessorEndpoint.java
+     * @throws Exception
+     */
+    @Test
+    public void postPutHappyCase() throws Exception {
+
+	//populate secondary index
+	Put idxPut = new Put("EFG1".getBytes());
+	idxPut.addColumn("a".getBytes(), "genome_accesion_number".getBytes(), "EFB1".getBytes());
+	secondaryIdxTable.put(idxPut);
+	secondaryIdxTable.flushCommits();
+	
+        Put tablePut = new Put("EFG1".getBytes());
+        tablePut.addColumn( "e".getBytes(), "genome_accession_number".getBytes(), "EFG1".getBytes());
+        primaryTable.put(tablePut);
+        primaryTable.flushCommits();
+
+        Get get = new Get("EFB1".getBytes());
+        Result result = otherTable.get(get);
+        List<Cell> cells = result.listCells();
+        Cell cell = cells.get(0);
+        String ck = new String(CellUtil.cloneRow(cell));
+        String cf = new String(CellUtil.cloneFamily(cell));
+        String cq = new String(CellUtil.cloneQualifier(cell));
+
+        Assert.assertEquals( "We expect the column family from the rippled table","eg", cf);
+        Assert.assertEquals( "We expect the column to contain the rowkey for a target table", "genome_accession_number", cq);
+        Assert.assertEquals( "We expect the rowkey in the rippled table to be the source rowkey", "EFG1", ck);
+    }
+}
