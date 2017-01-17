70b94e77be528d780f3f035025dd2c07b3cf1a11
Steen Manniche <steen@manniche.net>
2017-01-17 14:09:26 +0100

    invoking the rabbitmq functionality

--- hbase-mq-data-rippler/src/main/java/net/nzcorp/hbase/mq_data_rippler/MQDataRippler.java
+++ hbase-mq-data-rippler/src/main/java/net/nzcorp/hbase/mq_data_rippler/MQDataRippler.java
@@ -1,15 +1,17 @@
 package net.nzcorp.hbase.mq_data_rippler;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.List;
-import java.util.concurrent.TimeoutException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.CellUtil;
-import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.client.Mutation;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Durability;
 import org.apache.hadoop.hbase.CoprocessorEnvironment;
 import org.apache.hadoop.hbase.coprocessor.BaseRegionObserver;
 import org.apache.hadoop.hbase.coprocessor.ObserverContext;
@@ -27,14 +29,12 @@ import com.rabbitmq.client.Channel;
 @SuppressWarnings("unused")
 public class MQDataRippler extends BaseRegionObserver {
 
-    private Connection conn;
+    private org.apache.hadoop.hbase.client.Connection conn;
     private ConnectionFactory factory;
     private String destinationTable;
     private String secCF;
     private String sourceColumn;
     private static final Log LOGGER = LogFactory.getLog(net.nzcorp.hbase.mq_data_rippler.MQDataRippler.class);
-    private String mq_ip;
-
 
     @Override
     public void start(CoprocessorEnvironment env) throws IOException {
@@ -47,15 +47,16 @@ public class MQDataRippler extends BaseRegionObserver {
          * the above configuration element
          */
 
-        conn = ConnectionFactory.createConnection( env.getConfiguration() );
+        conn = org.apache.hadoop.hbase.client.ConnectionFactory.createConnection( env.getConfiguration() );
 
-        mq_ip = env.getConfiguration().get("mq_ip");
+        final String mq_ip = env.getConfiguration().get("mq_ip");
 
         factory = new ConnectionFactory();
         factory.setHost(mq_ip);
         //factory.setConnectionTimeout();
 
 
+        destinationTable = env.getConfiguration().get("destination_table");
         secCF = env.getConfiguration().get("secondary_idx_cf");
         sourceColumn = env.getConfiguration().get("source_column");
         LOGGER.info("Initializing secondary indexer with destination table "+destinationTable+" and looking for "+sourceColumn);
@@ -70,8 +71,25 @@ public class MQDataRippler extends BaseRegionObserver {
             throws IOException
     {
         try {
+            /**
+             * The Mutation.getCellList is the method we need, as we want to get all qualifiers of a given column family
+             * Unfortunately, the method is package-private, and we only have access to the subclass `Put` here, so we
+             * access the method by using the Reflection API. Please note that this is a last resort tactic and that we
+             * will try to argue with the HBase projects, that there is a use case for exposing this method in the
+             * public API. As `Mutation::getCellList` is not in the public API, the method could disappear without
+             * deprecation warnings (or warnings at all). It will be in 2.0.0, as far as we know.
+             */
+            final Method meth = Mutation.class.getDeclaredMethod("getCellList", byte[].class);
+            meth.setAccessible(true);
+            final List<Cell> list_of_cells = (List<Cell>) meth.invoke(put, "e".getBytes());
 
-	    final List<Cell> cells = put.get(Bytes.toBytes("e"), Bytes.toBytes(sourceColumn));
+            if (list_of_cells.isEmpty()) {
+                LOGGER.info("No cells in this transaction");
+                return;
+            }
+            LOGGER.debug("Found " + Integer.toString(list_of_cells.size()) + " cells in Put");
+
+            final List<Cell> cells = put.get(Bytes.toBytes("e"), Bytes.toBytes(sourceColumn));
             if (cells.isEmpty()) {
                 return;
             }
@@ -88,12 +106,12 @@ public class MQDataRippler extends BaseRegionObserver {
                  */
             }
             Connection connection = null;
-            try {
-                connection = factory.newConnection();
-            } catch (TimeoutException e) {
-                LOGGER.error(String.format("Timeout while trying to connect to MQ@%s", mq_ip));
-            }
+
+            connection = factory.newConnection();
+//                LOGGER.error(String.format("Timeout while trying to connect to MQ@%s", mq_ip));
             Channel channel = connection.createChannel();
+            channel.queueDeclare(destinationTable, true, false, false, null);
+            channel.basicPublish("",destinationTable, false, null, );
 
 
         } catch (IllegalArgumentException ex) {
@@ -101,8 +119,12 @@ public class MQDataRippler extends BaseRegionObserver {
             //In order not to drop its marbles when the HBase server throws an IllegalArgumentException, we allow the
             // coprocessor to continue operating, thereby allowing the HBase RS to continue operating.
             // This exception is throws before the secondary index table is opened, so just move along
-            return;
-
+        } catch (NoSuchMethodException e) {
+            LOGGER.error("In trying to acquire reference to the Mutation::getCellList, an error occurred", e);
+        } catch (IllegalAccessException e) {
+            LOGGER.error("In trying to assign the reference to the Mutation::getCellList to a variable, an error occurred", e);
+        } catch (InvocationTargetException e) {
+            LOGGER.error("In trying to invoke the Mutation::getCellList, an error occurred", e);
         }
 
     }
--- hbase-mq-data-rippler/src/test/java/net/nzcorp/hbase/mq_data_rippler/MQDataRipplerTest.java
+++ hbase-mq-data-rippler/src/test/java/net/nzcorp/hbase/mq_data_rippler/MQDataRipplerTest.java
@@ -1,23 +1,29 @@
 package net.nzcorp.hbase.mq_data_rippler;
 
+import com.rabbitmq.client.Channel;
 import org.apache.hadoop.hbase.*;
-import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.coprocessor.CoprocessorHost;
 import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.junit.*;
 
-import java.util.ArrayList;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
 
 public class MQDataRipplerTest {
 
     private static final TableName primaryTableName = TableName.valueOf("assembly");
-    private static final TableName otherTableName = TableName.valueOf("genome_index");
-    private HTable primaryTable;
-    private HTable otherTable;
+    private org.apache.hadoop.hbase.client.HTable primaryTable;
 
     private HBaseTestingUtility testingUtility = new HBaseTestingUtility();
 
@@ -36,133 +42,52 @@ public class MQDataRipplerTest {
         /*
          * Load the coprocessor on the region
          */
-
         testingUtility.getConfiguration().setStrings(
                 CoprocessorHost.USER_REGION_COPROCESSOR_CONF_KEY,
                 MQDataRippler.class.getName());
+
+        /*
+         * Set configuration values (emulating the alter_async command for installing coprocessors
+         */
         testingUtility.getConfiguration().setStrings("destination_table", "genome_index");
         testingUtility.getConfiguration().setStrings("source_column", "genome_accession_number");
         testingUtility.getConfiguration().setStrings("secondary_idx_cf", "a");
 
-        // Get random port number
+        // Get random port number for hbase master and RS
         testingUtility.getConfiguration().setInt("hbase.regionserver.port", HBaseTestingUtility.randomFreePort());
         testingUtility.getConfiguration().setInt("hbase.master.info.port", HBaseTestingUtility.randomFreePort());
 
         testingUtility.startMiniCluster();
 
+        /*
+         * Create table with column families
+         */
         byte[][] ba = new byte[2][];
         ba[0] = "e".getBytes();
         ba[1] = "foo".getBytes();
 
-        primaryTable = (HTable) testingUtility.createTable(primaryTableName, ba);
-
-        otherTable = (HTable) testingUtility.createTable(otherTableName, "a");
+        primaryTable = (org.apache.hadoop.hbase.client.HTable) testingUtility.createTable(primaryTableName, ba);
     }
 
     @After
     public void after() throws Exception {
         testingUtility.deleteTable(primaryTableName);
-        testingUtility.deleteTable(otherTableName);
         primaryTable = null;
-        otherTable = null;
         testingUtility.shutdownMiniCluster();
         testingUtility = null;
     }
 
-
-    /**
-     * maybe: https://hbase.apache.org/testdevapidocs/src-html/org/apache/hadoop/hbase/coprocessor/TestRegionObserverStacking.ObserverB.html
-     * https://github.com/sematext/HBaseHUT/blob/master/src/test/java/com/sematext/hbase/hut/cp/TestHBaseHutCps.java
-     * https://qnalist.com/questions/136205/hbase-coprocessor-unit-testing
-     * https://github.com/apache/hbase/blob/master/hbase-endpoint/src/test/java/org/apache/hadoop/hbase/coprocessor/TestRegionServerCoprocessorEndpoint.java
-     * @throws Exception
-     */
     @Test
     public void postPutHappyCase() throws Exception {
+        ConnectionFactory mockConnectionFactory = mock(ConnectionFactory.class);
+        Connection mockConnection = mock(Connection.class);
+        Channel mockChannel = mock(Channel.class);
 
-        Put tablePut = new Put("EFB1".getBytes());
-        tablePut.addColumn( "e".getBytes(), "genome_accession_number".getBytes(), "EFG1".getBytes());
-        //tablePut.add(CellUtil.createCell("e".getBytes(), "genome_accession_number".getBytes(), "EFG1".getBytes()));
-        primaryTable.put(tablePut);
-        primaryTable.flushCommits();
+        when(mockConnectionFactory.newConnection(any(ExecutorService.class), anyString())).thenReturn(mockConnection);
+        when(mockConnection.isOpen()).thenReturn(true);
+        when(mockConnection.createChannel()).thenReturn(mockChannel);
 
-        Get get = new Get("EFG1".getBytes());
-        Result result = otherTable.get(get);
-        List<Cell> cells = result.listCells();
-        Cell cell = cells.get(0);
-        String ck = new String(CellUtil.cloneRow(cell));
-        String cf = new String(CellUtil.cloneFamily(cell));
-        String cq = new String(CellUtil.cloneQualifier(cell));
-
-        Assert.assertEquals( "We expect the column family from the secondary index","a", cf);
-        Assert.assertEquals( "We expect the column to contain the rowkey for a target table", "EFB1", cq);
-        Assert.assertEquals( "We expect the rowkey in the secondary index to be the source rowkey", "EFG1", ck);
-    }
-
-    @Test
-    public void postPutMultiplePuts() throws Exception {
-        Put tablePut1 = new Put( "EFB1".getBytes() );
-        tablePut1.addColumn("e".getBytes(), "genome_accession_number".getBytes(), "EFG2".getBytes());
-        Put tablePut2 = new Put( "EFB2".getBytes() );
-        tablePut2.addColumn("e".getBytes(), "genome_accession_number".getBytes(), "EFG2".getBytes());
-
-        List<Put> puts = new ArrayList<Put>();
-        puts.add(tablePut1);
-        puts.add(tablePut2);
-
-        primaryTable.put( puts );
-        primaryTable.flushCommits();
-
-        Get get = new Get("EFG2".getBytes());
-        Result result = otherTable.get(get);
-        List<Cell> cells = result.listCells();
-
-        Assert.assertEquals("We should have received two columns on EFG2", 2, cells.size());
-
-        Cell cell = cells.get(0);
-        String ck = new String(CellUtil.cloneRow(cell));
-        String cf = new String(CellUtil.cloneFamily(cell));
-        String cq = new String(CellUtil.cloneQualifier(cell));
-
-        Assert.assertEquals( "We expect the column family from the secondary index","a", cf);
-        Assert.assertEquals( "We expect the column to contain the rowkey for a target table", "EFB1", cq);
-        Assert.assertEquals( "We expect the rowkey in the secondary index to be the source rowkey", "EFG2", ck);
-
-        cell = cells.get(1);
-        ck = new String(CellUtil.cloneRow(cell));
-        cf = new String(CellUtil.cloneFamily(cell));
-        cq = new String(CellUtil.cloneQualifier(cell));
-
-        Assert.assertEquals( "We expect the column family from the secondary index","a", cf);
-        Assert.assertEquals( "We expect the column to contain the rowkey for a target table", "EFB2", cq);
-        Assert.assertEquals( "We expect the rowkey in the secondary index to be the source rowkey", "EFG2", ck);
-    }
-
-    @Test
-    public void postPutToDifferentCFDoesNotIndex() throws Exception{
-        Put tablePut = new Put( "EFB1".getBytes() );
-        tablePut.addColumn("foo".getBytes(), "genome_accession_number".getBytes(), "EFG3".getBytes());
-
-        primaryTable.put( tablePut );
-        primaryTable.flushCommits();
-
-        Get get = new Get("EFG3".getBytes());
-        Result result = otherTable.get(get);
-        List<Cell> cells = result.listCells();
-
-        Assert.assertNull("We should not have received any columns on EFG3", cells);
-    }
-
-    @Test
-    public void handlingPutWithEmptySourceValue() throws Exception {
-        Put tablePut = new Put("EFB1".getBytes());
-        tablePut.addColumn( "e".getBytes(), "genome_accession_number".getBytes(), "".getBytes());
-        primaryTable.put(tablePut);
-        primaryTable.flushCommits();
-
-        Get get = new Get("EFG1".getBytes());
-        Result result = otherTable.get(get);
-        Assert.assertNull("There should be no secondary index for EFG1", result.listCells());
+        when(mockChannel.isOpen()).thenReturn(true);
     }
 
 }
