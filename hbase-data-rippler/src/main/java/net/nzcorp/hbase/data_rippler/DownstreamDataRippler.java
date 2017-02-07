0f2c968258a7130c5a77f0cfec7a63c94fbd190a
Steen Manniche <steen@manniche.net>
2017-02-07 11:09:53 +0100

    removing unused code

--- hbase-data-rippler/src/main/java/net/nzcorp/hbase/data_rippler/DownstreamDataRippler.java
+++ hbase-data-rippler/src/main/java/net/nzcorp/hbase/data_rippler/DownstreamDataRippler.java
@@ -59,13 +59,18 @@ public class DownstreamDataRippler extends BaseRegionObserver {
      */
     private boolean f_debug;
 
+    /**
+     * The fully qualified connection string to the amqp server
+     *
+     * e.g.  amqp://guest:guest@rabbitmq:5672/airflow
+     */
+
     private static final double NANOS_TO_SECS = 1000000000.0;
     private String amq_address;
 
     private com.rabbitmq.client.ConnectionFactory factory;
     private com.rabbitmq.client.Connection connection;
     private com.rabbitmq.client.Channel channel;
-    private boolean build_cache;
 
     @Override
     public void start(CoprocessorEnvironment env) throws IOException {
@@ -142,7 +147,6 @@ public class DownstreamDataRippler extends BaseRegionObserver {
             throw new CoprocessorException(toe.getMessage());
         }
 
-        build_cache = false;
         LOGGER.info("Initializing data rippler copying values from column family " + sourceCF + " to " + destinationTable + ":" + targetCf);
         LOGGER.info("Using secondary index " + secondaryIndexTable + ", column family: "+ secondaryIndexCF);
 
@@ -154,8 +158,6 @@ public class DownstreamDataRippler extends BaseRegionObserver {
                         final WALEdit edit,
                         final Durability durability_enum)
             throws IOException {
-        Table table = null;
-        Table secTable = null;
         try {
             LOGGER.trace("Entering DDR#postPut");
             long startTime = System.nanoTime();
@@ -163,7 +165,7 @@ public class DownstreamDataRippler extends BaseRegionObserver {
 
             final String sourceTable = observerContext.getEnvironment().getRegionInfo().getTable().getNameAsString();
 
-            /**
+            /*
              * The Mutation.getCellList is the method we need, as we want to get all qualifiers of a given column family
              * Unfortunately, the method is package-private, and we only have access to the subclass `Put` here, so we
              * access the method by using the Reflection API. Please note that this is a last resort tactic and that we
@@ -181,10 +183,6 @@ public class DownstreamDataRippler extends BaseRegionObserver {
             }
             LOGGER.debug("Found " + Integer.toString(list_of_cells.size()) + " cells in Put");
 
-
-            final Map<String, List<byte[]>> keysCache = new HashMap<String, List<byte[]>>();
-
-
             if (f_debug) {
                 for (Cell cell : list_of_cells) {
                     final byte[] rowKey = CellUtil.cloneRow(cell);
@@ -193,25 +191,14 @@ public class DownstreamDataRippler extends BaseRegionObserver {
             }
 
             for (Cell cell : list_of_cells) {
-                final byte[] rowKey = CellUtil.cloneRow(cell);
-
-                if(build_cache) {
-                    if (!keysCache.containsKey(new String(rowKey))) {
-                        lapTime = (double) (System.nanoTime()) / NANOS_TO_SECS;
-
-                        LOGGER.debug(String.format("building cache for %s", new String(rowKey)));
-                        keysCache.put(new String(rowKey), getTargetRowkeys(rowKey, secTable));
-                        lapTime = (System.nanoTime() / NANOS_TO_SECS) - lapTime;
-                        LOGGER.debug(String.format("Built cache in %f seconds", lapTime));
-                    }
-                }
 
                 AMQP.BasicProperties headers = new AMQP.BasicProperties.Builder().
                         contentType(ContentType.JSON).
                         priority(1).
                         deliveryMode(DeliveryType.PERSISTENT).build();
-                String message = constructJsonObject(keysCache, cell, rowKey);
+                String message = constructJsonObject(cell);
 
+                final byte[] rowKey = CellUtil.cloneRow(cell);
 
                 channel.basicPublish(destinationTable,
                                      new String(rowKey),
@@ -232,66 +219,31 @@ public class DownstreamDataRippler extends BaseRegionObserver {
 
         } catch (NoSuchMethodException e) {
             LOGGER.error("In trying to acquire reference to the Mutation::getCellList, an error occurred", e);
+            throw new CoprocessorException(e.getMessage());
         } catch (IllegalAccessException e) {
             LOGGER.error("In trying to assign the reference to the Mutation::getCellList to a variable, an error occurred", e);
+            throw new CoprocessorException(e.getMessage());
         } catch (InvocationTargetException e) {
             LOGGER.error("In trying to invoke the Mutation::getCellList, an error occurred", e);
-        } finally {
-            /**
-             * Clean up resources that may have been left open
-             */
-            if (table != null) {
-                table.close();
-            }
-            if (secTable != null) {
-                secTable.close();
-            }
+            throw new CoprocessorException(e.getMessage());
         }
     }
 
-    private String constructJsonObject(Map<String, List<byte[]>> keysCache, Cell cell, byte[] rowKey) {
-        /**
+    private String constructJsonObject(Cell cell) {
+        /*
          * If build_cache == true, we retrieve target keys from the secondary index and send them along in the
          * json message
         */
 
-        JSONObject jo = new JSONObject();
-
-        List<String> ls = new ArrayList<>();
-        if(build_cache) {
-            for( byte[] ba : keysCache.get(new String(rowKey)) ){
-                ls.add(new String(ba));
-            }
-        }
+         JSONObject jo = new JSONObject();
 
          jo.put("column_family", targetCf);
          jo.put("column_qualifier", new String(CellUtil.cloneQualifier(cell)));
          jo.put("column_value",new String(CellUtil.cloneValue(cell)));
          jo.put("secondary_index", secondaryIndexTable);
          jo.put("secondary_index_cf", secondaryIndexCF);
-         jo.put("target_keys", ls);
 
         return jo.toString();
-    }
-
-    private List<byte[]> getTargetRowkeys(byte[] rowKey, Table secTable) throws IOException {
-
-        List<byte[]> targetKeys = new ArrayList<byte[]>();
-        Result result = secTable.get(new Get(rowKey));
-        List<Cell> cellList = result.listCells();
-
-	if( cellList != null && cellList.size() > 0 ) {
-	    for( Cell cell: cellList ) {
-		byte[] cf = CellUtil.cloneFamily(cell);
-		if (Arrays.equals( cf, secondaryIndexCF.getBytes())) {
-		    LOGGER.debug(String.format("got column %s", new String(CellUtil.cloneQualifier(cell))));
-		    targetKeys.add(CellUtil.cloneQualifier(cell));
-		}
-	    }
-	} else {
-	    LOGGER.warn( String.format("Found no targetKeys for %s", new String(rowKey)));
-	}
-	    return targetKeys;
     }
 
     @Override
