6c413e8a1e891cb8f8dbd2708abd46c035d52746
Troels Nielsen <tns@novozymes.com>
2017-03-27 19:02:25 +0200

    Make upgrade.py python2 and 3 compatible

--- bin/upgrade.py
+++ bin/upgrade.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python
+#
+# Usage: 
+# upgrade.py with no arguments will increase the minor version 
+# in every pom.xml file.
+#
+# With one argument it will change the version to that in every pom.xml file
+# in the current project.
+# 
+# Additionally the property, <shade_append> will be set to a packagized version 
+# of the version string. 
+
+
 from lxml import etree
 from glob import glob
 import os
 import sys
 import re
 
+
 NAMESPACE = 'http://maven.apache.org/POM/4.0.0'
 NAMESPACES = {'xmlns': NAMESPACE}
 PROJECT_DIRECTORY = os.path.join(os.path.dirname(__file__), '..')
 PARENT_POM_FILE = os.path.join(PROJECT_DIRECTORY, 'pom.xml')
 SHADE_APPEND_PROPERTY = 'shade_append'
+
+def usage():
+    print("%s <version>" % (sys.argv[0], ))
+    print("Version is a maven version string of format: <n>.<n>.<n>[-<suffix>]")
+    sys.exit(1)
 
 def update_cell(xml, xpath, value):
     res = xml.xpath(xpath, namespaces=NAMESPACES)
@@ -18,7 +36,7 @@ def update_cell(xml, xpath, value):
 def parse_version(version):
     match = re.match('^(\d+)\.(\d+)\.(\d+)((?:-.*)|)$', version)
     if not match:
-        raise ValueError("Could not parse version: %s, expected format: <num>.<num>.<num>-<suffix>", 
+        raise ValueError("Could not parse version: %s, expected format: <n>.<n>.<n>[-<suffix>]", 
                          version)
     return (int(match.group(1)), int(match.group(2)), int(match.group(3)), match.group(4))
 
@@ -27,7 +45,9 @@ parent_pom = etree.parse(PARENT_POM_FILE)
 version = parent_pom.xpath('/xmlns:project/xmlns:version/text()', namespaces=NAMESPACES)
 version = version[0]
 
-if len(sys.argv) == 2:
+if len(sys.argv) > 2:
+    usage()
+elif len(sys.argv) == 2:
     new_version = sys.argv[1]
     parsed_new_version = parse_version(new_version)
 else:
@@ -52,12 +72,9 @@ for module in modules:
     pom = os.path.join(PROJECT_DIRECTORY, module, 'pom.xml')
     pom_xml = etree.parse(pom)
     update_cell(pom_xml, '/xmlns:project/xmlns:parent/xmlns:version', new_version_string)
-    with open(pom, 'w') as fh:
-        fh.write(etree.tostring(pom_xml))
+    pom_xml.write(pom)
 
-with open(PARENT_POM_FILE, 'w') as fh:
-    fh.write(etree.tostring(parent_pom))
+
+parent_pom.write(PARENT_POM_FILE)
 
 print("Upgraded to version: %s" % (new_version_string, ))
-
-    
